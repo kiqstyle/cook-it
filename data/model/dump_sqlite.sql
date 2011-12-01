ea866e946db009ff3142d47d4e4cf925be2690ac
parzych.kamil <parzych.kamil@gmail.com>
2011-12-01 20:18:21 +0000

    DONE - issue 8: Create a tool to easily import data with recipes into the databse http://code.google.com/p/cook-it/issues/detail?id=8  * Excel sheet with data commited  * CSV files must be exported with UTF-8 encoding  * CSV files tested in SQLite Manager (Firefox plugin) - works after delete all rows from a table (otherwise an error about constraints appears  * Data moved from SQL initial script to CSV files

--- data/model/dump_sqlite.sql
+++ data/model/dump_sqlite.sql
@@ -34,8 +34,8 @@ create table Ingredients
 (
    id                   int not null,
    Kin_id               int not null,
-   name_en              varchar(80) not null,
-   name_pl              varchar(80) not null,
+   name_en              varchar(80),
+   name_pl              varchar(80),
    desc_en              varchar(255),
    desc_pl              varchar(255),
    primary key (id),
@@ -49,8 +49,8 @@ create table Kind
 (
    id                   int not null,
    Kin_id               int,
-   kind_en              varchar(50) not null,
-   kind_pl              varchar(50) not null,
+   kind_en              varchar(50),
+   kind_pl              varchar(50),
    desc_en              varchar(255),
    desc_pl              varchar(255),
    primary key (id),
@@ -64,8 +64,8 @@ create table Quantity
 (
    Uni_id               int not null,
    Ing_id               int not null,
-   Rec_id               int not null,
-   pos_id               int not null,
+   Rec_id               int,
+   pos_id               int,
    quantity             varchar(10),
    primary key (Uni_id, Ing_id, Rec_id, pos_id),
    constraint FK_Quantity foreign key (pos_id) references Fridge_Content (pos_id) on delete restrict on update restrict,
@@ -80,8 +80,8 @@ create table Quantity
 create table Recipe_Type
 (
    id                   int not null,
-   name_en              varchar(20) not null,
-   name_pl              varchar(20) not null,
+   name_en              varchar(20),
+   name_pl              varchar(20),
    desc_en              varchar(255),
    desc_pl              varchar(255),
    primary key (id)
@@ -94,8 +94,8 @@ create table Recipes
 (
    id                   int not null,
    Rec_id               int not null,
-   title_en             varchar(50) not null,
-   title_pl             varchar(50) not null,
+   title_en             varchar(50),
+   title_pl             varchar(50),
    text_en              varchar(5000),
    text_pl              varchar(5000),
    created              varchar(10),
@@ -109,15 +109,14 @@ create table Recipes
 create table Units
 (
    id                   int not null,
-   name_en              varchar(40) not null,
-   name_pl              varchar(40) not null,
-   abbreviation_en      varchar(20) not null,
-   abbreviation_pl      varchar(20) not null,
+   name_en              varchar(40),
+   name_pl              varchar(40),
+   abbreviation_en      varchar(20),
+   abbreviation_pl      varchar(20),
    desc_en              varchar(255),
    desc_pl              varchar(255),
    primary key (id)
 );
-
 
 
 /*==============================================================*/
@@ -131,40 +130,52 @@ create table Units
 /*==============================================================*/
 /* Table: Ingredients                                           */
 /*==============================================================*/
+/*
 INSERT INTO "Ingredients" (id, Kin_id, name_en, name_pl, desc_en, desc_pl) VALUES ("1", "1", "", "Jaja", "", "Jaja");
 INSERT INTO "Ingredients" (id, Kin_id, name_en, name_pl, desc_en, desc_pl) VALUES ("2", "1", "", "Mas�o", "", "Mas�o");
 INSERT INTO "Ingredients" (id, Kin_id, name_en, name_pl, desc_en, desc_pl) VALUES ("3", "1", "", "S�l", "", "S�l");
+*/
 
 /*==============================================================*/
 /* Table: Kind                                                  */
 /*==============================================================*/
+/*
 INSERT INTO "Kind" (id, Kin_id, kind_en, kind_pl, desc_en, desc_pl) VALUES ("1", "", "", "Brak rodzaju", "", "Brak rodzaju");
+*/
 
 /*==============================================================*/
 /* Table: Quantity                                              */
 /*==============================================================*/
+/*
 INSERT INTO "Quantity" (Uni_id, Ing_id, Rec_id, pos_id, quantity) VALUES ("1", "1", "1", "", "3");
 INSERT INTO "Quantity" (Uni_id, Ing_id, Rec_id, pos_id, quantity) VALUES ("4", "2", "1", "", "1");
+*/
 
 /*==============================================================*/
 /* Table: Recipe_Type                                           */
 /*==============================================================*/
+/*
 INSERT INTO "Recipe_Type" (id, name_en, name_pl, desc_en, desc_pl) VALUES ("1", "", "Zupa", "", "Zupa");
 INSERT INTO "Recipe_Type" (id, name_en, name_pl, desc_en, desc_pl) VALUES ("2", "", "Danie g��wne", "", "Danie g��wne");
 INSERT INTO "Recipe_Type" (id, name_en, name_pl, desc_en, desc_pl) VALUES ("3", "", "Sur�wka", "", "Sur�wka");
 INSERT INTO "Recipe_Type" (id, name_en, name_pl, desc_en, desc_pl) VALUES ("4", "", "Sa�atka", "", "Sa�atka");
 INSERT INTO "Recipe_Type" (id, name_en, name_pl, desc_en, desc_pl) VALUES ("5", "", "Przystawka", "", "Przystawka");
 INSERT INTO "Recipe_Type" (id, name_en, name_pl, desc_en, desc_pl) VALUES ("6", "", "Przek�ska", "", "Przek�ska");
+*/
 
 /*==============================================================*/
 /* Table: Recipes                                               */
 /*==============================================================*/
+/*
 INSERT INTO "Recipes" (id, Rec_id, title_en, title_pl, text_en, text_pl, created) VALUES ("1", "2", "", "Jajecznica", "", "Jaja wybi� na talerzu i roztrzepa�, osoli� do smaku. Na patelni rozgrza� �y�k� mas�a. Na rozgrzane mas�o wyla� jaja i mieszaj�c energicznie a� do uzyskania odpowiedniej konsystencji.", "2011-11-12");
+*/
 
 /*==============================================================*/
 /* Table: Units                                                 */
 /*==============================================================*/
+/*
 INSERT INTO "Units" (id, name_en, name_pl, abbreviation_en, abbreviation_pl, desc_en, desc_pl) VALUES ("1", "", "sztuka", "", "szt.", "", "");
 INSERT INTO "Units" (id, name_en, name_pl, abbreviation_en, abbreviation_pl, desc_en, desc_pl) VALUES ("2", "", "kilogram", "", "kg", "", "");
 INSERT INTO "Units" (id, name_en, name_pl, abbreviation_en, abbreviation_pl, desc_en, desc_pl) VALUES ("3", "", "kostka", "", "kostka", "", "");
 INSERT INTO "Units" (id, name_en, name_pl, abbreviation_en, abbreviation_pl, desc_en, desc_pl) VALUES ("4", "", "�y�ka", "", "�y�.", "", "");
+*/
